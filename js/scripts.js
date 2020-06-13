9c82eba4462a5e375ddb369365a94d663fea533e
Nathan Kendrick <nathan.kendrick@twc.com>
2020-06-13 16:42:46 -0400

    Created Test Pokemon List
    - Removed existing code
    - Added an array consisting of Pokemon Objects for a test list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-alert('Hello world');
-var favoriteFood = 'fish';
-document.write(favoriteFood);
+var pokemonList = [
+    { name: 'Eevee', pokedexNumber: 133, height: 0.3, weight: 6.5, types: ['Normal'] },
+    { name: 'Vaporeon', pokedexNumber: 134, height: 1, weight: 29, types: ['Water'] },
+    { name: 'Jolteon', pokedexNumber: 135, height: 0.8, weight: 24.5, types: ['Electric'] },
+    { name: 'Flareon', pokedexNumber: 136, height: 0.9, weight: 25, types: ['Fire'] },
+    { name: 'Espeon', pokedexNumber: 196, height: 0.9, weight: 26.5, types: ['Psychic'] },
+    { name: 'Umbreon', pokedexNumber: 197, height: 1, weight: 27, types: ['Dark'] },
+    { name: 'Leafeon', pokedexNumber: 470, height: 1, weight: 25.5, types: ['Grass'] },
+    { name: 'Glaceon', pokedexNumber: 471, height: 0.8, weight: 25.9, types: ['Ice'] },
+    { name: 'Sylveon', pokedexNumber: 700, height: 1, weight: 23.5, types: ['Fairy'] }
+]
