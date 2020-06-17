568ed10c3dabbc077063db95fc7c532c41512e1e
Nathan Kendrick <nathan.kendrick@twc.com>
2020-06-17 11:51:28 -0400

    Styling and Function Updates
    - Made code for array easier to read.
    - Added Loop to print Name, Type, and Weight of array to document.
    - Added condition to call out small Pokemon from the list.
    - Added a logo and styling to app.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,71 @@
 var pokemonList = [
-    { name: 'Eevee', pokedexNumber: 133, height: 0.3, weight: 6.5, types: ['Normal'] },
-    { name: 'Vaporeon', pokedexNumber: 134, height: 1, weight: 29, types: ['Water'] },
-    { name: 'Jolteon', pokedexNumber: 135, height: 0.8, weight: 24.5, types: ['Electric'] },
-    { name: 'Flareon', pokedexNumber: 136, height: 0.9, weight: 25, types: ['Fire'] },
-    { name: 'Espeon', pokedexNumber: 196, height: 0.9, weight: 26.5, types: ['Psychic'] },
-    { name: 'Umbreon', pokedexNumber: 197, height: 1, weight: 27, types: ['Dark'] },
-    { name: 'Leafeon', pokedexNumber: 470, height: 1, weight: 25.5, types: ['Grass'] },
-    { name: 'Glaceon', pokedexNumber: 471, height: 0.8, weight: 25.9, types: ['Ice'] },
-    { name: 'Sylveon', pokedexNumber: 700, height: 1, weight: 23.5, types: ['Fairy'] }
+{
+    pokemonName: 'Eevee',
+    pokedexNumber: 133,
+    pokemonHeight: 0.3,
+    pokemonWeight: 6.5,
+    pokemonTypes: ['Normal'],
+},
+{
+    pokemonName: 'Vaporeon',
+    pokedexNumber: 134,
+    pokemonHeight: 1.0,
+    pokemonWeight: 29,
+    pokemonTypes: ['Water']
+},
+{
+    pokemonName: 'Jolteon',
+    pokedexNumber: 135,
+    pokemonHeight: 0.8,
+    pokemonWeight: 24.5,
+    pokemonTypes: ['Electric'],
+},
+{
+    pokemonName: 'Flareon',
+    pokedexNumber: 136,
+    pokemonHeight: 0.9,
+    pokemonWeight: 25,
+    pokemonTypes: ['Fire'],
+},
+{ 
+    pokemonName: 'Espeon',
+    pokedexNumber: 196,
+    pokemonHeight: 0.9,
+    pokemonWeight: 26.5,
+    pokemonTypes: ['Psychic'],
+},
+{
+    pokemonName: 'Umbreon',
+    pokedexNumber: 197,
+    pokemonHeight: 1.0,
+    pokemonWeight: 27,
+    pokemonTypes: ['Dark'],
+},
+{
+    pokemonName: 'Leafeon',
+    pokedexNumber: 470,
+    pokemonHeight: 1.0,
+    pokemonWeight: 25.5,
+    pokemonTypes: ['Grass'],
+},
+{
+    pokemonName: 'Glaceon',
+    pokedexNumber: 471,
+    pokemonHeight: 0.8,
+    pokemonWeight: 25.9,
+    pokemonTypes: ['Ice'],
+},
+{
+    pokemonName: 'Sylveon',
+    pokedexNumber: 700,
+    pokemonHeight: 1.0,
+    pokemonWeight: 23.5,
+    pokemonTypes: ['Fairy'],
+}
 ]
+
+for (var i = 0; i < pokemonList.length; i++) {
+    document.write( '<p>', (pokemonList[i].pokemonName), ' is ', (pokemonList[i].pokemonTypes), ' type ', ' and is ' , (pokemonList[i].pokemonHeight), ' meters in height. ', '</p>');
+var pokemonHeight = [i];
+if (pokemonHeight < 0.5) document.write('(Wow, ' + pokemonList[i].pokemonName + ' is really small!)');
+}
