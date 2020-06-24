cba7f38879b7e14527a25a6ac7308b80da710835
Nathan Kendrick <nathan.kendrick@twc.com>
2020-06-24 12:26:27 -0400

    Wrapped pokemonList in an IIFE
    - Created pokemonRepository to contain pokemonList.
    - Assigned getAll and add functions within pokemonRepository.
    - Updated forEach loop to access pokemonList through getAll function within pokemonRepository IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,86 @@
-var pokemonList = [
-{
-    pokemonName: 'Eevee',
-    pokedexNumber: 133,
-    pokemonHeight: 0.3,
-    pokemonWeight: 6.5,
-    pokemonTypes: ['Normal'],
-},
-{
-    pokemonName: 'Vaporeon',
-    pokedexNumber: 134,
-    pokemonHeight: 1.0,
-    pokemonWeight: 29,
-    pokemonTypes: ['Water']
-},
-{
-    pokemonName: 'Jolteon',
-    pokedexNumber: 135,
-    pokemonHeight: 0.8,
-    pokemonWeight: 24.5,
-    pokemonTypes: ['Electric'],
-},
-{
-    pokemonName: 'Flareon',
-    pokedexNumber: 136,
-    pokemonHeight: 0.9,
-    pokemonWeight: 25,
-    pokemonTypes: ['Fire'],
-},
-{ 
-    pokemonName: 'Espeon',
-    pokedexNumber: 196,
-    pokemonHeight: 0.9,
-    pokemonWeight: 26.5,
-    pokemonTypes: ['Psychic'],
-},
-{
-    pokemonName: 'Umbreon',
-    pokedexNumber: 197,
-    pokemonHeight: 1.0,
-    pokemonWeight: 27,
-    pokemonTypes: ['Dark'],
-},
-{
-    pokemonName: 'Leafeon',
-    pokedexNumber: 470,
-    pokemonHeight: 1.0,
-    pokemonWeight: 25.5,
-    pokemonTypes: ['Grass'],
-},
-{
-    pokemonName: 'Glaceon',
-    pokedexNumber: 471,
-    pokemonHeight: 0.8,
-    pokemonWeight: 25.9,
-    pokemonTypes: ['Ice'],
-},
-{
-    pokemonName: 'Sylveon',
-    pokedexNumber: 700,
-    pokemonHeight: 1.0,
-    pokemonWeight: 23.5,
-    pokemonTypes: ['Fairy'],
-}
-]
+var pokemonRepository = (function () {
+    var pokemonList = [
+        {
+            pokemonName: 'Eevee',
+            pokedexNumber: 133,
+            pokemonHeight: 0.3,
+            pokemonWeight: 6.5,
+            pokemonTypes: ['Normal'],
+        },
+        {
+            pokemonName: 'Vaporeon',
+            pokedexNumber: 134,
+            pokemonHeight: 1.0,
+            pokemonWeight: 29,
+            pokemonTypes: ['Water']
+        },
+        {
+            pokemonName: 'Jolteon',
+            pokedexNumber: 135,
+            pokemonHeight: 0.8,
+            pokemonWeight: 24.5,
+            pokemonTypes: ['Electric'],
+        },
+        {
+            pokemonName: 'Flareon',
+            pokedexNumber: 136,
+            pokemonHeight: 0.9,
+            pokemonWeight: 25,
+            pokemonTypes: ['Fire'],
+        },
+        { 
+            pokemonName: 'Espeon',
+            pokedexNumber: 196,
+            pokemonHeight: 0.9,
+            pokemonWeight: 26.5,
+            pokemonTypes: ['Psychic'],
+        },
+        {
+            pokemonName: 'Umbreon',
+            pokedexNumber: 197,
+            pokemonHeight: 1.0,
+            pokemonWeight: 27,
+            pokemonTypes: ['Dark'],
+        },
+        {
+            pokemonName: 'Leafeon',
+            pokedexNumber: 470,
+            pokemonHeight: 1.0,
+            pokemonWeight: 25.5,
+            pokemonTypes: ['Grass'],
+        },
+        {
+            pokemonName: 'Glaceon',
+            pokedexNumber: 471,
+            pokemonHeight: 0.8,
+            pokemonWeight: 25.9,
+            pokemonTypes: ['Ice'],
+        },
+        {
+            pokemonName: 'Sylveon',
+            pokedexNumber: 700,
+            pokemonHeight: 1.0,
+            pokemonWeight: 23.5,
+            pokemonTypes: ['Fairy'],
+        }
+    ];
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    function getAll() {
+      return pokemonList;
+    }
+    
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
 
 function displayPokemonData(data) {
     document.write('<p>' + data.pokemonName + ' is ' + data.pokemonTypes + ' type, and is ' + data.pokemonHeight + ' meters in height. ' + '</p>');
     if (data.pokemonHeight < 0.5) document.write('(Wow, ' + data.pokemonName + ' is really small!)');
 }
-pokemonList.forEach(displayPokemonData);
+pokemonRepository.getAll().forEach(displayPokemonData);
